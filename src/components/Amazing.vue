def5dffa152245665607514ac07d711f88c8f5e2
gbemme <gbemikp@gmail.com>
2020-02-14 11:25:50 +0100

    Next and Amazing Page Updated

---
+++ src/components/Amazing.vue
@@ -0,0 +1,78 @@
+<template>
+    <div  class="container">
+        <div class="row  amazing-body">
+        
+            <div class="row nav-amazing">
+
+                <div class="col-md-6 semi-amazing">
+                    <img src="./semicolon-logo.svg">
+              <!-- <h2 class="logo">Semicolon</h2> -->
+                </div>
+
+                <div class="col-md-6 hello-amazing"> 
+                        <h5>Hi Tolani &#x25BC;</h5>
+
+                </div>
+                    
+
+
+            </div>
+
+                <div class="row">
+                
+
+                    <div >
+
+
+                     </div>
+                                
+
+                    <div class="col-sm-4 box-amazing">
+                        <div class="row">
+                                    <div class="circle-amazing">
+
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="amazing">
+                                        <strong>Amazing</strong>
+                                    
+                                    </div>
+                                    <div class="msg-amazing">
+
+                                        <h5>You have successfully completed the Semicolon Aptitude Test. We will get back to you, very soon.</h5>
+                                
+                                    </div>
+                                 </div>
+
+                                <div class="row">
+                                <div class="btn">
+                                    <button type="submit" class="back-amazing" >&#x2190;Submit</button>
+                                </div>
+                                </div>
+
+
+                  </div>
+
+
+
+                        <div >
+
+
+                        </div>
+
+
+
+
+                </div>
+        
+             
+        
+        
+        
+        
+        </div>
+        </div>
+       
+</template>
+<style src="./CreatePassword.css" scoped></style>
---
+++ src/components/Next.vue
@@ -0,0 +1,69 @@
+<template>
+    <div  class="container">
+         <div class="row  amazing-body">
+            <div class="row nav-amazing">
+               
+
+                <div class="col-md-6 semi-amazing">
+                    <img src="./semicolon-logo.svg">
+              <!-- <h2 class="logo">Semicolon</h2> -->
+                </div>
+
+                <div class="col-md-6 hello-amazing"> 
+                        <h5>Hi Tolani &#x25BC;</h5>
+
+                </div>
+                    
+
+
+            </div>
+
+                <div class="row">
+
+                    <div class="col-sm-4 box-amazing">
+                                <div class="row">
+                                    <div class="circle-amazing">
+
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="next-amazing">
+                                  <strong>Next Steps</strong>
+                                    
+                                    </div>
+                                   
+                                   
+                                    <div class="msg-amazing">
+
+                                        <h5>Your questionnaire has been submitted.
+                                        Click the link below to take your aptitude test.</h5>
+                                
+                                    </div>
+                                 </div>
+
+                                <div class="row">
+                                    <div class="btn">
+                                        <button type="submit" class="back-amazing" >&#x2190;Take Tests</button>
+                                    </div>
+                                </div>
+
+
+                           
+
+
+
+
+                </div>
+        
+                    </div>
+
+
+            </div>
+        
+        
+        
+        
+        
+        </div>
+</template>
+<style src="./CreatePassword.css" scoped></style>
